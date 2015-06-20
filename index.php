99ec5b7eeb197a34df7deb49d3c3d355d8cd9427
Simon Jentzsch <simon@jentzschfamily.net>
2015-06-20 21:10:13 +0200

    fixed index.php

--- index.php
+++ index.php
@@ -124,7 +124,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <div class="footer">
 	 <div class="container">
 		 <div class="ftr-logo">
-			 <a class="wow bounceIn" data-wow-delay="0.5s" href="index.html"><img src="images/logo2.png" alt=""/></a>
+			 <a class="wow bounceIn" data-wow-delay="0.5s" href="index.php"><img src="images/logo2.png" alt=""/></a>
 		 </div>
 		 <div class="copy-right wow bounceInUp" data-wow-delay="0.5s">
 			 <p>Copyright &#169; 2015 slock.it . All Rights Reserved.</p>
