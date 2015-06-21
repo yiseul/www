6d21c474748a95c7ae0e68307ab7f4db5c3b7b90
Simon Jentzsch <simon@jentzschfamily.net>
2015-06-21 19:47:55 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php 
-#if(strpos($_SERVER['HTTP_REFERER']),'index.php') !== false) { 
+if(strpos($_SERVER['HTTP_REFERER']),'slock') !== false) { 
     $to = "info@xonia7.de"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
@@ -12,5 +12,5 @@
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-#}
+}
 ?>
