ebf8ac359abd26f157283c178f9331be1bd932c5
Simon Jentzsch <simon@jentzschfamily.net>
2015-06-21 19:31:44 +0200

    created mailer

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php 
+#if(strpos($_SERVER['HTTP_REFERER']),'index.php') !== false) { 
+    $to = "info@xonia7.de"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = "slock.it-request: ".;
+    $subject2 = "Copy of your slock.it form submission";
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['txt'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['txt'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+#}
+?>
