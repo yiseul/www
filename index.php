dc86275b1e93d573b64cb0d146ad15caba86e66d
Simon Jentzsch <simon@jentzschfamily.net>
2015-06-30 10:38:56 +0200

    added google analytics

--- index.php
+++ index.php
@@ -149,5 +149,16 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 </script>
 <a href="#to-top" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
 <!----> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-52631731-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
  </body>
  </html>
