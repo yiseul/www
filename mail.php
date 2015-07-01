f3f3f8d2f1ca05cba94f094d8f0f83bc74e5f10d
Simon Jentzsch <simon@jentzschfamily.net>
2015-07-01 07:31:30 +0200

    changed form-email

--- mail.php
+++ mail.php
@@ -5,10 +5,10 @@ if(strpos($_SERVER['HTTP_REFERER'],'slock') !== false) {
     $name = $_POST['name'];
     $subject = "slock.it-request: ".$_POST['subject'];
     $subject2 = "Copy of your slock.it form submission";
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['txt'];
+    $message = $name . " ( $from ) wrote the following:" . "\n\n" . $_POST['txt'];
     $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['txt'];
 
-    $headers = "From:" . $from;
+    $headers = "";//"From:" . $from;
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
