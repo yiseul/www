8c8a27adac553c6a7888b2899b352c8ef66819ce
Simon Jentzsch <simon@jentzschfamily.net>
2015-08-20 14:25:15 +0200

    added php

---
+++ php/mail.php
@@ -0,0 +1,16 @@
+<?php 
+if(strpos($_SERVER['HTTP_REFERER'],'slock') !== false) { 
+    $to = "info@slock.it"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = "slock.it-request: ".$_POST['subject'];
+    $subject2 = "Copy of your slock.it form submission";
+    $message = $name . " ( $from ) wrote the following:" . "\n\n" . $_POST['txt'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['txt'];
+
+    $headers = "";//"From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+}
+?>
