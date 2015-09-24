4bbd7903d4e466886b833971bdcbe5648a9445af
Simon Jentzsch <simon@jentzschfamily.net>
2015-09-24 16:28:51 +0200

    fixed escaping

--- php/mail.php
+++ php/mail.php
@@ -14,10 +14,11 @@ if(strpos($_SERVER['HTTP_REFERER'],'slock') !== false) {
     
     
     
-    $data = "payload=" . json_encode(array(
+    $data = array("payload"=> json_encode(array(
                 "text"          =>  "$subject\n$message",
                 "icon_emoji"    =>  ":incoming_envelope:"
-            ));
+            )));
+	
 	
     $ch = curl_init("https://hooks.slack.com/services/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");
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
@@ -25,12 +26,11 @@ if(strpos($_SERVER['HTTP_REFERER'],'slock') !== false) {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $result = curl_exec($ch);
     curl_close($ch);
-    mail("simon@jentzschfamily.net","mailer","send : $data   \n result: ".$result,$headers);
+    if ($result=="Payload was not valid JSON") {
+        mail("simon@jentzschfamily.net","mailer-error","send : $data   \n result: ".$result,$headers);
+    }
         
-    //mail($to,$subject,$message,$headers);
-    //mail($from,$subject2,$message2,$headers); // sends a copy of the message to the sender
-        
-    
-    
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers); // sends a copy of the message to the sender
 }
 ?>
